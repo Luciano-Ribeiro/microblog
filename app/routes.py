9c7e01eae4db13609fef55361e35ac0e86b5fe54
Luciano Ribeiro <luciano.melo@pontotel.com.br>
2022-08-10 11:58:54 -0300

    Utilizando jinja2 com templates html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Olá, mundo!"
+    user = {'username':'Luciano'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Mais um dia de chuva em Joinville'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Qual será o próximo filme da Marvel?'
+        },
+        {
+            'author': {'username': 'Jack'},
+            'body': 'Jogar em consoles ou pc?'
+        }
+    ]
+    return render_template("index.html",title='Home', user=user, posts=posts)
+
