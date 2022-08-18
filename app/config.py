9652f5dfdc4f7ffca1382f31a6b9d304620df0ea
Luciano Ribeiro <luciano.melo@pontotel.com.br>
2022-08-18 11:04:48 -0300

    Adicionando paginação de posts

--- app/config.py
+++ app/config.py
@@ -5,14 +5,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     FLASK_APP = "microblog.py"
     SECRET_KEY = "senha-secreta"
+    # Config para SQLAlchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Config para servidor de email
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']  # Adicione seu email
+    # config para paginação
+    POSTS_PER_PAGE = 25
 
 
